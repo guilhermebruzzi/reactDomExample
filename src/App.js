0767beb9b72ae01dc7218c4180819c2976ba5efe
Guilherme Bruzzi <guibruzzi@gmail.com>
2016-10-09 22:47:32 -0300

    Use inline styles on App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+const styles = {
+  app: {
+    textAlign: 'center',
+  },
+  appLogo: {
+    animation: 'App-logo-spin infinite 20s linear',
+    height: 80,
+  },
+  appHeader: {
+    backgroundColor: '#222',
+    height: 150,
+    padding: 20,
+    color: 'white',
+  },
+  appIntro: {
+    fontSize: 'large',
+  },
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div style={styles.app}>
+        <div style={styles.appHeader}>
+          <img src={logo} style={styles.appLogo} alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p style={styles.appIntro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
