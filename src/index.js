ae3f67dff8907397633b00453a438ee290eab8ae
Guilherme Bruzzi <guibruzzi@gmail.com>
2016-10-22 00:23:34 -0200

    Removing code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactDOMServer from 'react-dom/server';
 import App from './App';
 import './index.css';
-
-// wrong way
-
-// const appRawMarkup = ReactDOMServer.renderToString(<App helpMessage='To get started, edit <code>src/App.js</code> and save to reload.' />);
-//
-// console.log('appRawMarkup', appRawMarkup);
-//
-// document.getElementById('root').innerHTML = appRawMarkup;
-
-// correct way with VirtualDom optimization
 
 ReactDOM.render(
   <App helpMessage='To get started, edit <code>src/App.js</code> and save to reload.' />,
